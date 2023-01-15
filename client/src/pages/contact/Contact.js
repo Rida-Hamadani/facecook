270e27e5dc2980caa17b96b0b937c454a01cd632
Rida <mridahamadani@gmail.com>
2023-01-15 06:23:27 +0200

    added homepage

--- client/src/pages/contact/Contact.js
+++ client/src/pages/contact/Contact.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import saul from './saul.jpg';
 import './Contact.css';
 
 export class Contact extends Component {
   render() {
     return (
         <div className="contact">
-          Click <a href="mailto: mridahamadani@gmail.com" >here</a> to send us an email.
+          <img alt="Saul Goodman" src={saul} width="300px" length="200px"/>
+          <p>Click <a href="mailto: mridahamadani@gmail.com" >here</a> to send us an email.</p>
         </div>
     )
   }
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,46 +1,37 @@
 import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import hazmat from './hazmat.jpg';
+import gus from './gus.jpg';
 import './Home.css';
 
 class Home extends Component {
 
-  constructor(props) {
-
-    super(props);
-
-    this.state = {
-
-      response: null
-
-    };
-
-  }
-
-  componentDidMount() {
-
-    fetch('http://localhost:8888/products/featured')
-      .then(response => response.json())
-      .then(data => {
-
-        this.setState({
-          
-          response: data
-
-        });
-
-      });
-
-  }
-
   render() {
+    const user = localStorage.getItem("user").slice(1, -1);
     return (
      <Fragment>
-
-        {this.state.response ? (
-          <p>{JSON.stringify(this.state.response)}</p>
-        ) : (
-          <p>Loading...</p>
-        )}
-
+      <div className='home'>
+      <div className='welcome'>
+        Welcome{user ? ', ' + user + ', ': ' ' }to FaceCook, the #1 e-commerce company in the world!
+      </div>
+      <div className='grid-container'>
+      <div className='grid-item'>
+      <Link to='/shop'><p>Browse our top products, produced by the best cook, Walter White himself.</p></Link>
+      </div>
+      <div className='grid-item'>
+      <img src={hazmat} alt="Our labs" />
+      </div>
+      <div className='grid-item'>
+      <img src={gus} alt="Our CEO" />
+      </div>
+      <div className='grid-item'>
+      <Link to='/about'><p>Learn more about the history of our shop and the culture of our company.</p></Link>
+      </div>
+      </div>
+      <div className='welcome'>
+        <Link to='/contact'>Stay in touch.</Link>
+      </div>
+      </div>
      </Fragment>
     );
   }
